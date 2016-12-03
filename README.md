e06ff2be2b4029fc143c55a9552575addbf06131
saaineui <saaineui@gmail.com>
2016-12-03 13:06:15 -0800

    Frankenstein extension

---
+++ README.md
@@ -0,0 +1,6 @@
+# Single Speak
+## Chrome Browser Extension
+
+Chrome extension to replace euphemisms with clear terminology.
+
+Inspired by New Yorker essay by Chimamanda Adichie: http://www.newyorker.com/culture/cultural-comment/now-is-the-time-to-talk-about-what-we-are-actually-talking-about.
--- content.js
+++ content.js
@@ -1,20 +1,47 @@
-function getLinks() {
-  var links = document.querySelectorAll("a");
-  var results = [];
-  var seenLinks = {};
-  for (var i  = 0; i < links.length; ++i) {
-    var text = links[i].textContent;
-    if (text.length > 100)
-      text = text.substring(0, 100) + "...";
-    var link = links[i].href.replace(/(.*)#?/, "$1");
-    if (seenLinks[link])
-      continue;
-    seenLinks[link] = 1;
-    results.push({ href: link, text: text });
-  }
-  return results;
-};
+walk(document.body);
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  sendResponse(getLinks());
-});
+function walk(node) 
+{
+	// I stole this function from https://github.com/panicsteve/cloud-to-butt/ which stole it from:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	    || node.classList.indexOf('ace_editor') > -1) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\balt-right\b/g, "white supremacist right");
+	v = v.replace(/\bAlt-right\b/g, "White supremacist right");
+	v = v.replace(/\bAlt-Right\b/g, "White Supremacist Right");
+	v = v.replace(/\balt right\b/g, "white supremacist right");
+	v = v.replace(/\bAlt right\b/g, "White supremacist right");
+	v = v.replace(/\bAlt Right\b/g, "White Supremacist Right");
+	
+	textNode.nodeValue = v;
+}
